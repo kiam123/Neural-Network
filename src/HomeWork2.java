b65e45eeda05540064c1643a6f3f369b909742f3
kiam <kiam04@hotmail.com>
2016-10-07 03:39:57 +0800

    no message

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,6 +1,9 @@
 
 public class HomeWork2 {
-
+	/***
+	 * 類神經2 3 2 2
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/HomeWork4.java
+++ src/HomeWork4.java
@@ -1,6 +1,9 @@
 
 public class HomeWork4 {
-
+	/***
+	 * 類神經2 3 3 1
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
